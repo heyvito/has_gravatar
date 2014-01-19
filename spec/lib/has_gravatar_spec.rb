b15a893a8f87673f89886b3cf6275e4a4813ade9
Victor Gama <hey@vito.io>
2014-01-19 16:13:34 -0200

    Updated spec

--- spec/lib/has_gravatar_spec.rb
+++ spec/lib/has_gravatar_spec.rb
@@ -5,24 +5,26 @@ describe 'Model' do
     HasGravatar::VERSION.should_not be_nil
   end
   
+  let(:user) { User.first }
+  
   it 'should respond to email_gravatar method' do
-    User.first.should respond_to :email_gravatar
+    user.should respond_to :email_gravatar
   end
   
   it 'should return a valid gravatar url for the given email address' do
     # Hash calculated by md5.cz
-    User.first.email_gravatar.should eq 'http://gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8'
+    user.email_gravatar.should eq 'http://gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8'
   end
   
   it 'should support https gravatars' do
-    User.first.email_gravatar(secure: true).should start_with 'https://secure.gravatar'
+    user.email_gravatar(secure: true).should start_with 'https://secure.gravatar'
   end
   
   it 'should support default images' do
-    User.first.email_gravatar(default_avatar: 'http://example.com/public/default_gravatar.png').should include 'example.com'
+    user.email_gravatar(default_avatar: 'http://example.com/public/default_gravatar.png').should include 'example.com'
   end
   
   it 'should support gravatar size' do
-    User.first.email_gravatar(size: 128).should include '?s=128'
+    user.email_gravatar(size: 128).should include '?s=128'
   end
 end
