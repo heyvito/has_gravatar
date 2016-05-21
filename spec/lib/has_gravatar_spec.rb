df1211283f093467b6fbd6910f2957f120ccd266
Victor Gama <hey@vito.io>
2016-05-21 19:49:31 -0300

    Lint library

--- spec/lib/has_gravatar_spec.rb
+++ spec/lib/has_gravatar_spec.rb
@@ -1,29 +1,29 @@
 require 'spec_helper'
 
-describe 'Model' do 
+describe 'Model' do
   it 'should contain VERSION' do
     HasGravatar::VERSION.should_not be_nil
   end
-  
+
   let(:user) { User.first }
-  
+
   it 'should respond to email_gravatar method' do
     user.should respond_to :email_gravatar
   end
-  
+
   it 'should return a valid gravatar url for the given email address' do
     # Hash calculated by md5.cz
     user.email_gravatar.should eq 'http://gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8'
   end
-  
+
   it 'should support https gravatars' do
     user.email_gravatar(secure: true).should start_with 'https://secure.gravatar'
   end
-  
+
   it 'should support default images' do
     user.email_gravatar(default_avatar: 'http://example.com/public/default_gravatar.png').should include 'example.com'
   end
-  
+
   it 'should support gravatar size' do
     user.email_gravatar(size: 128).should include '?s=128'
   end
